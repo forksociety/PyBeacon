015118c38067158dd12afbfaea65b2524a477f71
PrabhanshuAttri <contact@prabhanshu.com>
2018-04-05 23:49:02 +0530

    v0.2.9.0

--- PyBeacon/__init__.py
+++ PyBeacon/__init__.py
@@ -1,6 +1,6 @@
 """Module configuration."""
 __title__ = 'PyBeacon'
-__version__ = '0.2.8.1'
+__version__ = '0.2.9.0'
 __build__ = 0x016
-__author__ = 'Nirmankarta'
+__author__ = 'forksociety'
 __license__ = 'GNU Affero General Public License v3 or later (AGPLv3+)'
